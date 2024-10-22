b993b0e4ce06bef0b96dc7b7da4fa2d5ddc25d79
LuizHFS-Codice <LuizHFS@al.insper.edu.br>
2024-10-22 19:01:32 -0300

    Joguete de teste

---
+++ Batalha Naval/Jogo.py
@@ -0,0 +1,18 @@
+#Jogo
+from funcoes import define_posicoes
+
+Tam=0 #Tamanho do Navio
+Lin=int(input("Linha escolhida> "))
+Col=int(input("Coluna Escolhida> "))
+Ori=input("Rotação da Embarcação (vertical ou horizontal)> ")
+Embarc=input("Qual o tipo de Embarcação> ")
+ListEmbarc=["porta-aviões","navio-tanque","contratorpedeiro","submarino"]
+if Embarc == ListEmbarc[0]:
+    Tam=4
+elif Embarc==ListEmbarc[1]:
+    Tam=3
+elif Embarc==ListEmbarc[2]:
+    Tam=2
+elif Embarc==ListEmbarc[3]:
+    Tam=1
+print(define_posicoes(Lin,Col,Ori,Tam))
