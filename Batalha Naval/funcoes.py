79952caf40d0998cf02f2113b3532b031641a2d2
LuizHFS-Codice <LuizHFS@al.insper.edu.br>
2024-10-23 20:08:19 -0300

    OBS

--- Batalha Naval/funcoes.py
+++ Batalha Naval/funcoes.py
@@ -52,7 +52,7 @@ def faz_jogada(tabuleiro, Lin, Col):
 
     # verificando  a posicao no tabuleiro com condiconais 
     if tabuleiro[Lin][Col] == 1:
-        tabuleiro[Lin][Col] = 'X' #Ou seja, acertou alguma coisa 
+        tabuleiro[Lin][Col] = 'X' #Ou seja, acertou alguma coisa (obs : usar "X")
     else:
          tabuleiro[Lin][Col] = '-' #errou e não acertou ninguém 
     
