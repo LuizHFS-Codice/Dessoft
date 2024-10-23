6bf53f1cb4cfd8abeb6ca88443eac165cde433e2
antonio-victor-99 <antonio.victor99@hotmail.com>
2024-10-23 19:01:20 -0300

    # Questão 2 (feita dia 22/10)

--- dev2.py
+++ dev2.py
@@ -1,1 +1,46 @@
-print('Luiz')
+def define_posicoes(Lin,Col,Ori,Tam):
+    lf=[]  #lista final
+    Cont=0 
+    Lis_Pos=0
+    #
+    if Ori=='horizontal':
+        for i in range(Tam):
+            Cont=Col+i
+            Lis_Pos=[Lin,Cont]
+            lf.append(Lis_Pos)
+        Cont=0
+        #######################
+    else:
+        Lis_Cont=range(Lin,Tam+1)
+        for i in range(Tam):
+            Cont=Lin+i
+            Lis_Pos=[Cont,Col]
+            lf.append(Lis_Pos)
+        ###
+    return lf
+#------------------------------------------------------------------------------------
+# Questão 2 (feita dia 22/10)
+def preenche_frota(frota, nome_navio, Lin, Col, Ori,Tam):
+
+    posicoes_navio = define_posicoes(Lin,Col, Ori, Tam)
+
+
+    if nome_navio in frota:
+        frota[nome_navio].append(posicoes_navio)
+    else: 
+        frota[nome_navio] = [posicoes_navio]
+    
+    return frota 
+
+frota = {}
+nome_navio = "navio-tanque"
+Lin = 6
+Col = 1
+Ori = "horizontal"
+Tam = 3
+
+resultado = preenche_frota(frota,nome_navio, Lin, Col, Ori, Tam)
+print(resultado)
+#---------------------------------------------------------------------------------
+
+
