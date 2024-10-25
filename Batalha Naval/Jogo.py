c16c6c4ccf193d9952b04993a7ec712298fbbbf4
LuizHFS-Codice <LuizHFS@al.insper.edu.br>
2024-10-25 19:39:43 -0300

    Teste do jogo

--- Batalha Naval/Jogo.py
+++ Batalha Naval/Jogo.py
@@ -10,11 +10,13 @@ from pprint import pprint
 import numpy as np
 
 Tam=4 #Tamanho do Navio
-Embarcações=['Submarino','Contratorpedeiro','Navio-Tanque','Porta-Aviões']#Lista de Embarcações
+Embarcações=['submarino','contratorpedeiro','navio-tanque','porta-aviões']#Lista de Embarcações
 Pos=0
 Lin=0
 Col=0
 Ori=0
+frota={}
+contagem=0
 while Tam!=0:#Contagem usando o tamanho como diferenciador
     print(f'Insira as informações referentes ao navio {Embarcações[Tam-1]} que possui o tamanho {Tam}')
     Embarc=Embarcações[Tam-1]#Embaracação do ciclo atual
@@ -26,18 +28,7 @@ while Tam!=0:#Contagem usando o tamanho como diferenciador
     "submarino": [],
     }
 #-------------------------------------------------------------------------------------------------------   
-    Lin=int(input("Linha escolhida> "))
-    Col=int(input("Coluna Escolhida> "))
-    if Tam>1:
-        Ori=int(input("Rotação da Embarcação (1 (Vertical) ou 2 (Horizontal))> "))
-        if Ori==1:
-            Ori='vertical'
-        else:
-            Ori='horizontal'
-    Check1=posicao_valida(frota,Lin,Col,Ori,Tam)
-#--------------------------------------------------------------------------------------------------------
-    while Check1 != True:#Caso não seja uma posição valida
-        print('Esta posição não está válida!')
+    if Tam==4:
         Lin=int(input("Linha escolhida> "))
         Col=int(input("Coluna Escolhida> "))
         if Tam>1:
@@ -48,11 +39,90 @@ while Tam!=0:#Contagem usando o tamanho como diferenciador
                 Ori='horizontal'
         Check1=posicao_valida(frota,Lin,Col,Ori,Tam)
 #--------------------------------------------------------------------------------------------------------
-    Pos=
-    
-
-
-Frota=preenche_frota(Frota,Embarc,Lin,Col,Ori,Tam)
-print(Frota)
-Tabuleiro=posiciona_frota(Frota)
-pprint(Tabuleiro)
+        while Check1 != True:#Caso não seja uma posição valida
+            print('Esta posição não está válida!')
+            Lin=int(input("Linha escolhida> "))
+            Col=int(input("Coluna Escolhida> "))
+            if Tam>1:
+                Ori=int(input("Rotação da Embarcação (1 (Vertical) ou 2 (Horizontal))> "))
+                if Ori==1:
+                    Ori='vertical'
+                else:
+                    Ori='horizontal'
+            Check1=posicao_valida(frota,Lin,Col,Ori,Tam)
+#--------------------------------------------------------------------------------------------------------
+        Pos=define_posicoes(Lin,Col,Ori,Tam)#Posição da Embarcação
+        frota[Embarc]+=Pos#Colocando a posição da embarcação dentro da Frota
+#-------------------------------------------------------------------------------------------------------- 
+    elif Tam==3:
+        while contagem<2:
+            Lin=int(input("Linha escolhida> "))
+            Col=int(input("Coluna Escolhida> "))
+            Ori=int(input("Rotação da Embarcação (1 (Vertical) ou 2 (Horizontal))> "))
+            if Ori==1:
+                Ori='vertical'
+            else:
+                Ori='horizontal'
+        Check1=posicao_valida(frota,Lin,Col,Ori,Tam)
+#--------------------------------------------------------------------------------------------------------
+        while Check1 != True:#Caso não seja uma posição valida
+            print('Esta posição não está válida!')
+            Lin=int(input("Linha escolhida> "))
+            Col=int(input("Coluna Escolhida> "))
+            Ori=int(input("Rotação da Embarcação (1 (Vertical) ou 2 (Horizontal))> "))
+            if Ori==1:
+                Ori='vertical'
+            else:
+                Ori='horizontal'
+            Check1=posicao_valida(frota,Lin,Col,Ori,Tam)
+#---------------------------------------------------------------------------------------------------------
+        Pos=define_posicoes(Lin,Col,Ori,Tam)#Posição da Embarcação
+        frota[Embarc]+=Pos#Colocando a posição da embarcação dentro da Frota
+#---------------------------------------------------------------------------------------------------------
+    elif Tam==2:
+        while contagem<3:
+            Lin=int(input("Linha escolhida> "))
+            Col=int(input("Coluna Escolhida> "))
+            Ori=int(input("Rotação da Embarcação (1 (Vertical) ou 2 (Horizontal))> "))
+            if Ori==1:
+                Ori='vertical'
+            else:
+                Ori='horizontal'
+        Check1=posicao_valida(frota,Lin,Col,Ori,Tam)
+#--------------------------------------------------------------------------------------------------------
+        while Check1 != True:#Caso não seja uma posição valida
+            print('Esta posição não está válida!')
+            Lin=int(input("Linha escolhida> "))
+            Col=int(input("Coluna Escolhida> "))
+            if Tam>1:
+                Ori=int(input("Rotação da Embarcação (1 (Vertical) ou 2 (Horizontal))> "))
+                if Ori==1:
+                    Ori='vertical'
+                else:
+                    Ori='horizontal'
+            Check1=posicao_valida(frota,Lin,Col,Ori,Tam)
+#---------------------------------------------------------------------------------------------------------
+        Pos=define_posicoes(Lin,Col,Ori,Tam)#Posição da Embarcação
+        frota[Embarc]+=Pos#Colocando a posição da embarcação dentro da Frota
+#----------------------------------------------------------------------------------------------------------
+    elif Tam==1:
+        while contagem<4:
+            Lin=int(input("Linha escolhida> "))
+        Col=int(input("Coluna Escolhida> "))
+        Check1=posicao_valida(frota,Lin,Col,Ori,Tam)
+#--------------------------------------------------------------------------------------------------------
+        while Check1 != True:#Caso não seja uma posição valida
+            print('Esta posição não está válida!')
+            Lin=int(input("Linha escolhida> "))
+            Col=int(input("Coluna Escolhida> "))
+            if Tam>1:
+                Ori=int(input("Rotação da Embarcação (1 (Vertical) ou 2 (Horizontal))> "))
+                if Ori==1:
+                    Ori='vertical'
+                else:
+                    Ori='horizontal'
+            Check1=posicao_valida(frota,Lin,Col,Ori,Tam)
+#---------------------------------------------------------------------------------------------------------
+        Pos=define_posicoes(Lin,Col,Ori,Tam)#Posição da Embarcação
+        frota[Embarc]+=Pos#Colocando a posição da embarcação dentro da Frota
+    Tam-=1
