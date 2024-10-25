b3c343682eb80a32822361b5be0fc601df8cdaf2
LuizHFS-Codice <LuizHFS@al.insper.edu.br>
2024-10-25 19:07:46 -0300

    Avanços

--- Batalha Naval/Jogo.py
+++ Batalha Naval/Jogo.py
@@ -1,4 +1,5 @@
 #Jogo
+from funcoes import define_posicoes
 from funcoes import preenche_frota
 from funcoes import faz_jogada
 from funcoes import posiciona_frota
@@ -9,16 +10,22 @@ from pprint import pprint
 import numpy as np
 
 Tam=4 #Tamanho do Navio
-Embarcações=['Submarino','Contratorpedeiro','Navio-Tanque','Porta-Aviões']
-while Tam!=0:
-    print(f'Insira as informações referentes ao navio {Embarcações[Tam]} que possui o tamanho {Tam}')
-    Embarc=Embarcações[Tam]
+Embarcações=['Submarino','Contratorpedeiro','Navio-Tanque','Porta-Aviões']#Lista de Embarcações
+Pos=0
+Lin=0
+Col=0
+Ori=0
+while Tam!=0:#Contagem usando o tamanho como diferenciador
+    print(f'Insira as informações referentes ao navio {Embarcações[Tam-1]} que possui o tamanho {Tam}')
+    Embarc=Embarcações[Tam-1]#Embaracação do ciclo atual
+#-------------------------------------------------------------------------------------------------------
     frota = {
     "porta-aviões":[],
     "navio-tanque":[],
     "contratorpedeiro":[],
     "submarino": [],
-    }     
+    }
+#-------------------------------------------------------------------------------------------------------   
     Lin=int(input("Linha escolhida> "))
     Col=int(input("Coluna Escolhida> "))
     if Tam>1:
@@ -27,9 +34,21 @@ while Tam!=0:
             Ori='vertical'
         else:
             Ori='horizontal'
-    Check1=posicao_valida()
-    
-    
+    Check1=posicao_valida(frota,Lin,Col,Ori,Tam)
+#--------------------------------------------------------------------------------------------------------
+    while Check1 != True:#Caso não seja uma posição valida
+        print('Esta posição não está válida!')
+        Lin=int(input("Linha escolhida> "))
+        Col=int(input("Coluna Escolhida> "))
+        if Tam>1:
+            Ori=int(input("Rotação da Embarcação (1 (Vertical) ou 2 (Horizontal))> "))
+            if Ori==1:
+                Ori='vertical'
+            else:
+                Ori='horizontal'
+        Check1=posicao_valida(frota,Lin,Col,Ori,Tam)
+#--------------------------------------------------------------------------------------------------------
+    Pos=
     
 
 
