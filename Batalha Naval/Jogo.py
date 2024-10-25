00d202eef93629606f124b79cab2d722911096af
LuizHFS-Codice <LuizHFS@al.insper.edu.br>
2024-10-25 18:44:25 -0300

    Jogo, preparação

--- Batalha Naval/Jogo.py
+++ Batalha Naval/Jogo.py
@@ -2,24 +2,37 @@
 from funcoes import preenche_frota
 from funcoes import faz_jogada
 from funcoes import posiciona_frota
+from funcoes import afundados
+from funcoes import posicao_valida
+
 from pprint import pprint
 import numpy as np
 
-Tam=0 #Tamanho do Navio
-Lin=int(input("Linha escolhida> "))
-Col=int(input("Coluna Escolhida> "))
-Ori=input("Rotação da Embarcação (vertical ou horizontal)> ")
-Embarc=input("Qual o tipo de Embarcação> ")
-ListEmbarc=["porta-aviões","navio-tanque","contratorpedeiro","submarino"]
-if Embarc == ListEmbarc[0]:
-    Tam=4
-elif Embarc==ListEmbarc[1]:
-    Tam=3
-elif Embarc==ListEmbarc[2]:
-    Tam=2
-elif Embarc==ListEmbarc[3]:
-    Tam=1
-Frota={}
+Tam=4 #Tamanho do Navio
+Embarcações=['Submarino','Contratorpedeiro','Navio-Tanque','Porta-Aviões']
+while Tam!=0:
+    print(f'Insira as informações referentes ao navio {Embarcações[Tam]} que possui o tamanho {Tam}')
+    Embarc=Embarcações[Tam]
+    frota = {
+    "porta-aviões":[],
+    "navio-tanque":[],
+    "contratorpedeiro":[],
+    "submarino": [],
+    }     
+    Lin=int(input("Linha escolhida> "))
+    Col=int(input("Coluna Escolhida> "))
+    if Tam>1:
+        Ori=int(input("Rotação da Embarcação (1 (Vertical) ou 2 (Horizontal))> "))
+        if Ori==1:
+            Ori='vertical'
+        else:
+            Ori='horizontal'
+    Check1=posicao_valida()
+    
+    
+    
+
+
 Frota=preenche_frota(Frota,Embarc,Lin,Col,Ori,Tam)
 print(Frota)
 Tabuleiro=posiciona_frota(Frota)
