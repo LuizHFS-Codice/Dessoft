53ce41c13124dff505dd3c836a19599ef80477ac
LuizHFS-Codice <LuizHFS@al.insper.edu.br>
2024-11-04 17:33:27 -0300

    Tela Inicial

---
+++ Pygame/Primeira Tela.py
@@ -0,0 +1,18 @@
+import pygame #Importação Pygame
+pygame.init #Inicialização do Pygame
+
+window=pygame.display.set_mode((824,596))
+pygame.display.set_caption('Voando pelo espaço')
+
+game=True
+
+Ptela=pygame.image.load('Assets/Imagens/Capa.png')
+
+while game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+        window.fill((255,255,255))
+        window.blit(Ptela,(0,0))
+        pygame.display.update()
+pygame.quit()
