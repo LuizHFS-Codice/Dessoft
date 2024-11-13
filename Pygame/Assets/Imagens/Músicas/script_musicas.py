3a23424f2e724b8a1a378a19f14a7c9acea440aa
antonio-victor-99 <antonio.victo99@hotmail.com>
2024-11-13 17:31:08 -0300

    Arquivos de Músicas/sons + Scripts para adicionar ao código

---
+++ Pygame/Assets/Imagens/Músicas/script_musicas.py
@@ -0,0 +1,6 @@
+#Musicas
+
+pygame.mixes.music.load("Assets\Imagens\Músicas\Daft Punk - One more time (8 bit music)(TheArtistX ).weba")
+pygame.mixer.music.paly(-1) # -1 dis pro python que a trilha vai executar infinitamente 
+                        
+                        
